0de2a381ef0f26f9122dc038cc9f86a9903bddb1
teju <tejutanvi773@gmail.com>
2020-08-06 11:35:59 +0530

    Added : Test Cases

--- src/test/java/com/josh/usersrestapi/UsersRestApiApplicationTests.java
+++ src/test/java/com/josh/usersrestapi/UsersRestApiApplicationTests.java
@@ -46,8 +46,6 @@ class UsersRestApiApplicationTests {
     private WebApplicationContext context;
     @MockBean
     private IUserServices userServices;
-   // @Autowired
-     //UserRepository userRepository;
 
     ObjectMapper objectMapper = new ObjectMapper();
 
@@ -95,26 +93,19 @@ class UsersRestApiApplicationTests {
         loginDto.setPassword("teju123");
         Response response = new Response(200,"User is Successfully Login","User is Successfully Login");
         when(userServices.login(ArgumentMatchers.any(LoginDto.class))).thenReturn(response);
-       // UserEntity userEntity = userRepository.findByEmail(loginDto.getUserEmail());
-       // System.out.println("User"+userEntity.isValidate());
-
-    mockMvc.perform(post("/login").content(new ObjectMapper().writeValueAsString(loginDto))
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.statuscode",is(response.getStatuscode())))
-            .andExpect(jsonPath("$.message",is(response.getMessage())))
-            .andExpect(jsonPath("$.data",is(response.getData())))
-            .andDo(print());
+        mockMvc.perform(post("/login").content(new ObjectMapper().writeValueAsString(loginDto))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statuscode",is(response.getStatuscode())))
+                .andExpect(jsonPath("$.message",is(response.getMessage())))
+                .andExpect(jsonPath("$.data",is(response.getData())))
+                .andDo(print());
     }
 
 
     @Test
     public void editUser() throws Exception{
-        EditUserDto editUserDto = new EditUserDto();
-       editUserDto.setFirstName("teju");
-       editUserDto.setLastName("surve");
-       editUserDto.setBirthdate(LocalDate.parse("2008-11-10"));
         UserEntity userEntity = new UserEntity();
         userEntity.setId(2);
         userEntity.setFirstName("teju");
@@ -122,20 +113,16 @@ class UsersRestApiApplicationTests {
         userEntity.setBirthdate(LocalDate.parse("2008-11-10"));
         userEntity.setEmail("teju123@gmail.com");
         userEntity.setPassword("teju123");
-        //System.out.println(new ObjectMapper().writeValueAsString(editUserDto));
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJ0ZWp1MTIzQGdtYWlsLmNvbSIsImlhdCI6MTU5NjEyMzIzNX0.ANBDh01HjrZCcQp3OA9ZvI563IkGbhlG0sg2LYoHzrk";
         Response response = new Response(200,"User is Successfully Updated",userEntity);
         String userDtoString = "{\"firstName\":\"teju\",\"lastName\":\"surve\", \"birthdate\":\"2008-11-10\" }";
-        when(userServices.updateUser(token,editUserDto)).thenReturn(response);
-       mockMvc.perform(post("/updateUser/"+"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJ0ZWp1MTIzQGdtYWlsLmNvbSIsImlhdCI6MTU5NjEyMzIzNX0.ANBDh01HjrZCcQp3OA9ZvI563IkGbhlG0sg2LYoHzrk")
+        when(userServices.updateUser(ArgumentMatchers.anyString(),ArgumentMatchers.any(EditUserDto.class))).thenReturn(response);
+        mockMvc.perform(post("/updateUser/"+"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJ0ZWp1MTIzQGdtYWlsLmNvbSIsImlhdCI6MTU5NjEyMzIzNX0.ANBDh01HjrZCcQp3OA9ZvI563IkGbhlG0sg2LYoHzrk")
                 .contentType(MediaType.APPLICATION_JSON_VALUE).content(userDtoString)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.statuscode",is(response.getStatuscode())))
-//                .andExpect(jsonPath("$.message",is(response.getMessage())))
-//                .andExpect(jsonPath("$.data.firstName",is(userEntity.getFirstName())))
+                .andExpect(jsonPath("$.statuscode",is(response.getStatuscode())))
+                .andExpect(jsonPath("$.message",is(response.getMessage())))
+                .andExpect(jsonPath("$.data.firstName",is(userEntity.getFirstName())))
                 .andDo(print());
-//        int status = mvcResult.getResponse().getStatus();
-//        assertEquals(200, status);
     }
 }
